895d95ddab9ae9a5e165d57c5225ef7345eed8ba
Noorhesham <noordragon2004@gmail.com>
2024-12-02 20:05:30 +0200

    page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,8 +1,9 @@
-"use client";
 import Login from "@/app/components/forms/LoginForm";
+import { unstable_setRequestLocale } from "next-intl/server";
 import React from "react";
 
-const page = () => {
+const page = async ({ params: { locale } }: { params: { locale: string } }) => {
+  await unstable_setRequestLocale(locale);
   return (
     <div className=" h-screen w-full flex justify-center items-center ">
       <Login />
