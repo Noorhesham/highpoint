c35c3f3a43141a4cba0d3c496e3f944b412cba50
Noorhesham <noordragon2004@gmail.com>
2025-02-12 07:48:09 +0200

    published only

--- app/components/NumberFooter.tsx
+++ app/components/NumberFooter.tsx
@@ -6,8 +6,8 @@ const NumberFooter = () => {
   const locale = useLocale();
   return (
     <div>
-      <a className=" items-center gap-2 flex !text-left underline" href="tel: +971 050 360 6133">
-        {locale === "ar" ? "6133 360 05 971+" : "+971 50 360 6133"}
+      <a className=" items-center gap-2 flex !text-left underline" href="tel: +971 50 360 6133">
+        {locale === "ar" ? "6133 360 50 971+" : "+971 50 360 6133"}
       </a>
       <a className=" items-center gap-2 flex !text-left underline" href="tel:050 360 6133">
         {locale === "ar" ? "6133 360 050" : "050 360 6133"}
