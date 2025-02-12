c103c988cf7e6727f58179731093014faf0696ae
Noorhesham <noordragon2004@gmail.com>
2025-02-12 18:51:22 +0200

    dynamic

--- app/[locale]/(pages)/about-us/page.tsx
+++ app/[locale]/(pages)/about-us/page.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import { getEntities } from "@/app/actions/actions";
 import HomeCover from "@/app/components/ui-visual/HomeCover";
 import Image from "next/image";
+export const dynamic = "force-dynamic";
 
 const page = async ({ params: { locale } }: { params: { locale: string } }) => {
   unstable_setRequestLocale(locale);
