ed25dd89075e20491cc550e65c07e74c8887ebb7
Noorhesham <noordragon2004@gmail.com>
2025-03-14 19:24:28 +0200

    seo error fix

--- app/[locale]/dashboard/courses/columns.tsx
+++ app/[locale]/dashboard/courses/columns.tsx
@@ -2,34 +2,13 @@
 
 import Actions from "@/app/components/dashboard/Actions";
 import dynamic from "next/dynamic";
-const PDFDownload = dynamic(() => import("@/app/components/ExportToPdf"), { ssr: false });
 import { CourseProps } from "@/app/models/Course";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ColumnDef } from "@tanstack/react-table";
-import CoursePage from "@/app/components/ExportToPdf";
-import ExportToPDF from "@/app/components/ExportToPdf";
 import NoSSR from "@/app/components/NoSSr";
-import ExportCoursesToPDF from "@/app/components/ExportCourseToPDf";
-import ExportToPdf from "@/app/components/ExportToPdf";
 import Link from "next/link";
 import { Pencil, PenIcon } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { useTranslations } from "next-intl";
+
 import { ExportPdfModal } from "@/app/components/ExportPdfModel";
 export const columns: ColumnDef<CourseProps>[] = [
   {
@@ -62,7 +41,7 @@ export const columns: ColumnDef<CourseProps>[] = [
     },
     cell: ({ row }) => {
       // Customize your display if needed.
-      const { en, ar } = row.original.name;
+      const { en, ar } = row.original.name || { en: "", ar: "" };
       return (
         <>
           <div>{en}</div>
